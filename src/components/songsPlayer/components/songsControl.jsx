84cc542ae2c3b34b17c9560dffc1dafac321156a
Garvit Bhatia <Garvit@Garvits-MacBook-Pro.local>
2023-11-16 10:23:08 +0530

    voice commands

--- src/components/songsPlayer/components/songsControl.jsx
+++ src/components/songsPlayer/components/songsControl.jsx
@@ -1,48 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Button from "./controlButton";
-import annyang from "annyang";
+// import annyang from "annyang";
 import "./songControl.css";
 
 const SongsControl = (props) => {
-  useEffect(() => {
-    // Initialize annyang with voice commands
-    if (annyang) {
-      annyang.setLanguage("en-US");
-      annyang.debug();
-      annyang.addCommands({
-        shuffle: () => props.shuffle(!props.shuffleActive),
-        previous: () => props.previousSong(),
-        play: () => {
-          props.playing ? props.pauseSong() : props.playSong();
-          console.log("play");
-        },
-        pause: () => {
-          props.playing ? props.pauseSong() : props.playSong();
-          console.log("play");
-        },
-        next: () => props.nextSong(),
-        repeat: () =>
-          props.repeatContext(props.repeatActive ? "off" : "context"),
-      });
-      annyang.start({ autoRestart: true, continuous: false }); // Auto-restart and non-continuous mode
-      console.log("annyang started!");
-    }
+  // useEffect(() => {
+  //   // Initialize annyang with voice commands
+  //   if (annyang) {
+  //     annyang.setLanguage("en-US");
+  //     annyang.debug();
+  //     annyang.addCommands({
+  //       shuffle: () => props.shuffle(!props.shuffleActive),
+  //       previous: () => props.previousSong(),
+  //       play: () => {
+  //         props.playing ? props.pauseSong() : props.playSong();
+  //         console.log("play");
+  //       },
+  //       pause: () => {
+  //         props.playing ? props.pauseSong() : props.playSong();
+  //         console.log("play");
+  //       },
+  //       next: () => props.nextSong(),
+  //       repeat: () =>
+  //         props.repeatContext(props.repeatActive ? "off" : "context"),
+  //     });
+  //     annyang.start({ autoRestart: true, continuous: false }); // Auto-restart and non-continuous mode
+  //     console.log("annyang started!");
+  //   }
 
-    return () => {
-      if (annyang) {
-        annyang.abort();
-      }
-    };
-  }, [
-    props.shuffleActive,
-    props.previousSong,
-    props.playing,
-    props.pauseSong,
-    props.playSong,
-    props.nextSong,
-    props.repeatActive,
-    props.repeatContext,
-  ]);
+  //   return () => {
+  //     if (annyang) {
+  //       annyang.abort();
+  //     }
+  //   };
+  // }, [
+  //   props.shuffleActive,
+  //   props.previousSong,
+  //   props.playing,
+  //   props.pauseSong,
+  //   props.playSong,
+  //   props.nextSong,
+  //   props.repeatActive,
+  //   props.repeatContext,
+  // ]);
 
   return (
     <div className="song-control">
